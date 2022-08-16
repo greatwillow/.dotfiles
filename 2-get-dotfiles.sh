6eea1421dce27b1e1e248e1684731f3953c3b7f7
Greg Denys <gregory.g.denys@gmail.com>
2022-08-15 22:34:54 -0400

    fix bug in get dotfiles script

--- 2-get-dotfiles.sh
+++ 2-get-dotfiles.sh
@@ -8,6 +8,7 @@
 gh_user="greatwillow"
 gh_repo=".dotfiles"
 gh_branch="main"
+dotfiles_url="https://github.com/${gh_user}/${gh_repo}/archive/refs/heads/${gh_branch}.tar.gz"
 
 #===============================================================================
 # Functions
@@ -15,21 +16,17 @@ gh_branch="main"
 
 source _common_functions
 
-get_dotfiles_url() {
-	print_line "You are currently set to load dotfiles from the default repository and branch."
+query_for_alternate_dotfiles_url() {
+	print_line "You are currently set to load dotfiles from $dotfiles_url"
 	print_line "Would you like to continue with the default selection? (y or n)"
-	read use_default_dotfiles_and_branch
+	read use_default_dotfiles_url
 
-	if [[ $use_default_dotfiles_and_branch != "y" ]]; then
-		print_line "What is the dotfiles github user name?"
-		read gh_user
-		print_line "What is the dotfiles repository name?"
-		read gh_repo
-		print_line "What is the dotfiles branch you would like to checkout?"
+	if [[ $use_default_dotfiles_url != "y" ]]; then
+		print_line "What is the dotfiles url you would like to use?"
+		read dotfiles_url
+		print_line "What branch of this repository would you like to use?"
 		read gh_branch
 	fi
-
-	return "https://github.com/${gh_user}/${gh_repo}/archive/refs/heads/${gh_branch}.tar.gz"
 }
 
 download_and_extract_dotfiles() {
@@ -50,5 +47,5 @@ download_and_extract_dotfiles() {
 cd $HOME
 current_user=$(whoami)
 print_line "You are currently now logged in as $current_user with home path at $HOME"
-dotfiles_url=$(get_dotfiles_url)
+query_for_alternate_dotfiles_url
 download_and_extract_dotfiles $dotfiles_url
