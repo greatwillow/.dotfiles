b834ba3ae1d510e2623f14686940005354475a20
Greg Denys <gregory.g.denys@gmail.com>
2022-08-15 22:26:55 -0400

    fix bug in get dotfiles script

--- 2-get-dotfiles.sh
+++ 2-get-dotfiles.sh
@@ -50,5 +50,5 @@ download_and_extract_dotfiles() {
 cd $HOME
 current_user=$(whoami)
 print_line "You are currently now logged in as $current_user with home path at $HOME"
-dotfiles_url=get_dotfiles_url
+dotfiles_url=$(get_dotfiles_url)
 download_and_extract_dotfiles $dotfiles_url
